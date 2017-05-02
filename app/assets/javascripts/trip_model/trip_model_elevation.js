f8d00fab000b8e267556a1ded7cdd7ee3a030db2
Jake Kelly <jakemkelly84@gmail.com>
2017-05-02 16:51:52 -0700

    Add comment to cumulative gain

--- app/assets/javascripts/trip_model/trip_model_elevation.js
+++ app/assets/javascripts/trip_model/trip_model_elevation.js
@@ -1,3 +1,7 @@
+// Considered saving cumulativeGain as a value and updating value on each new ping,
+// but the pings are draggable during creation and during edit.  Needs to re-calc every time
+// a ping is dragged/updated. 
+
 TripModel.prototype.getCumulativeGain = function(elevations){
   var cumulativeGain = 0
   for (i = 0; i < elevations.length - 1; i++){
